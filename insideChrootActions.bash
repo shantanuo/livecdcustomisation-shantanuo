810858c685bb443353fa3ea6fad4def558e87a6d
James McKay <jjm538@uowmail.edu.au>
2014-11-21 16:02:12 +1100

    Initial commit: Add scripts

---
+++ insideChrootActions.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Stuff
+cp -a /custom/. /
+
+apt-get install ibus{,-m17n,,-gtk} im-config
+apt-get install ttf-devanagari-fonts
+#apt-get purge ubiquity
+
+wget http://aksharyogini.sudhanwa.com/download/Aksharyogini2Normal.ttf -O /usr/share/fonts/truetype/Aksharyogini2Normal.ttf
+fc-cache --system-only
+
+unopkg add --shared /custom/libre_office_4.oxt
+
+glib-compile-schemas /usr/share/glib-2.0/schemas/
+
