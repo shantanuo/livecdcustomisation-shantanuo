2035b4c76d8eda43e086583071dccbf59e375c19
James McKay <jjm538@uowmail.edu.au>
2014-11-21 16:30:40 +1100

    Add another font

--- insideChrootActions.bash
+++ insideChrootActions.bash
@@ -8,6 +8,7 @@ apt-get install ttf-devanagari-fonts
 #apt-get purge ubiquity
 
 wget http://aksharyogini.sudhanwa.com/download/Aksharyogini2Normal.ttf -O /usr/share/fonts/truetype/Aksharyogini2Normal.ttf
+wget http://saraswaticlasses.net/download/akshar.ttf -O /usr/share/fonts/truetype/akshar.ttf
 fc-cache --system-only
 
 unopkg add --shared /custom/libre_office_4.oxt
