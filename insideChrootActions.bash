05e746e8d97b49e0666a50c8aa9da7878187d3dd
Shantanu Oak <shantanu.oak@gmail.com>
2015-06-24 19:11:58 +0530

    added updated version with auto text entries

--- insideChrootActions.bash
+++ insideChrootActions.bash
@@ -10,7 +10,7 @@ apt-get install ttf-devanagari-fonts
 wget http://aksharyogini.sudhanwa.com/download/Aksharyogini2Normal.ttf -O /usr/share/fonts/truetype/Aksharyogini2Normal.ttf
 wget http://saraswaticlasses.net/download/akshar.ttf -O /usr/share/fonts/truetype/akshar.ttf
 fc-cache --system-only
-
+# wget the new version from http://extensions.libreoffice.org/extension-center/marathi-spellchecker/releases/1.3/libre_office_4_autotext.oxt
 unopkg add --shared /custom/libre_office_4.oxt
 
 glib-compile-schemas /usr/share/glib-2.0/schemas/
